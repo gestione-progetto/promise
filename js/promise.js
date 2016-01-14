13e8325d27e8a0692c84893460ea389ebb569aa7
Antonio Pierro <antonio.pierro@gmail.com>
2016-01-14 17:34:19 +0100

    Added a loading state on a button

--- js/promise.js
+++ js/promise.js
@@ -6,14 +6,21 @@ var loadData = function () {
         $.each(data, function () {
             $('.table').append('<tr><td>' + this.language + '</td><td>' + this.developer + '</td></tr>');
         });
-    }
+    };
 
+    var $btn = $(this);
+
+    $btn.button('loading');
     promise.success(function (data) {
         printData(data);
     });
 
     promise.fail(function (error) {
         $('.bg-danger').append('<br>error' + error.responseText);
+    });
+
+    promise.done(function () {
+        $btn.button('reset');
     });
 };
 
